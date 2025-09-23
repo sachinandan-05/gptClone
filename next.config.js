b7e122dfbade86354bbc97c947082b81bfb7a7d4
sachinandan-05 <sachinandan.priv05@gmail.com>
2025-09-23 14:28:35 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Convert import.meta.url to __dirname equivalent
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "./src"),
-    };
-    return config;
-  },
+  reactStrictMode: true,
+  output: 'standalone',
   poweredByHeader: false,
   eslint: {
     ignoreDuringBuilds: true,
@@ -31,6 +19,14 @@ const nextConfig = {
   env: {
     MEM0_API_KEY: process.env.MEM0_API_KEY,
   },
+  webpack: (config) => {
+    // Add path aliases
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
